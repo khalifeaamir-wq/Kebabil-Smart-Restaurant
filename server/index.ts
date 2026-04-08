add477453b6936aeef3baa5bea0b727ff8ee3c96
khalifeaamir-wq <khalifeaamir@gmail.com>
2026-04-08 23:18:23 +0530

    fix deployment error

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";
+// @ts-ignore
 import SQLiteStore from "connect-sqlite3";
 import cors from "cors";
 import { registerRoutes } from "./routes";
@@ -169,14 +170,8 @@ app.use((req, res, next) => {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = parseInt(process.env.PORT || "5000", 10);
-  httpServer.listen(
-    {
-      port,
-      host: "0.0.0.0",
-    },
-    () => {
-      log(`serving on port ${port}`);
-    },
-  );
+  const PORT = process.env.PORT || "3000";
+  httpServer.listen(Number(PORT), "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 })();
